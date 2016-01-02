ce91aae42ee641e2c84081d9bb9ad48c75628c9a
anastasiavixen <anastasiavixen@gmail.com>
2016-01-01 21:20:59 -0500

    solution for test one

---
+++ roman-numerals/roman-numerals.php
@@ -0,0 +1,14 @@
+<?php
+function toRoman($number) {
+	$numerals = [
+		1    => "I",
+		5    => "V",
+		10   => "X",
+		50   => "L",
+		100  => "C",
+		500  => "D",
+		1000 => "M",
+	];
+
+	return $numerals[$number];
+}
